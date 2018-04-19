8be63222eabece4575a3b0d074043684a5844150
teaside <dima.rykunov@hotmail.com>
2018-04-19 20:52:28 +0300

    123

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-// import {ImageZoomModule} from 'angular2-image-zoom';
+import {ImageZoomModule} from 'angular2-image-zoom';
 
 import { LayoutComponent } from './layout.component';
 import { LayoutRoutingModule } from './layout-routing.module';
@@ -14,9 +14,8 @@ import { UserComponent } from './verification-queue/user/user.component';
     CommonModule,
     LayoutRoutingModule,
     FormsModule,
-    ReactiveFormsModule
-    // ,
-    // ImageZoomModule
+    ReactiveFormsModule,
+    ImageZoomModule
   ],
   declarations: [LayoutComponent, NavbarComponent, SidebarComponent, UserComponent]
 })
